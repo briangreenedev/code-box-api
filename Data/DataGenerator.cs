6c4cde5a81bc66d7cf2bb1e587a7a72c4da73e78
briangreenedev <brian@cloversoftware.net>
2020-07-08 11:47:24 -0600

    Started data generator service to generate dummy data for testing.

---
+++ Data/DataGenerator.cs
@@ -0,0 +1,19 @@
+﻿using OutsideBoxApi.Models;
+using OutsideBoxApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutsideBoxApi.Data
+{
+    public class DataGenerator
+    {
+        protected readonly DatabaseContext context;
+        public DataGenerator(DatabaseContext context)
+        {
+            this.context = context;
+        }
+    }
+}
